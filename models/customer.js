fa372e8fd7873102c14901df8e87e5a060c24b60
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-08-21 21:17:59 -0400

    initial commit

---
+++ models/customer.js
@@ -0,0 +1,16 @@
+module.exports = function (sequelize, DataTypes) {
+    var Customer = sequelize.define("Customer", {
+        name: {
+            type: DataTypes.STRING,
+            validate: {
+                is: ["^[a-z]+$", 'i']
+            }
+        },
+    });
+
+    Customer.associate = (models) => {
+        Customer.hasMany(models.Burger, {as: 'Purchases', foreignKey: 'purchases_id'}, );
+    };
+
+    return Customer;
+};
